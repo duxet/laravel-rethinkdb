a24cc2687c28c10604ee05a6c1ff4db59c70b7ff
duxet <duxetlg@gmail.com>
2014-12-06 22:11:48 +0100

    Fix problem with tests broken by missing tables

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,9 +5,10 @@ class TestCase extends Orchestra\Testbench\TestCase {
     /**
      * Get package providers.
      *
+     * @param  Illuminate\Foundation\Application    $app
      * @return array
      */
-    protected function getPackageProviders()
+    protected function getPackageProviders($app)
     {
         return [
             'duxet\Rethinkdb\RethinkdbServiceProvider',
@@ -34,6 +35,10 @@ class TestCase extends Orchestra\Testbench\TestCase {
 
         // overwrite cache configuration
         $app['config']->set('cache.driver', 'array');
+
+        // FIXME: There should be better way of doing this.
+        if (!Schema::hasTable('items')) Schema::create('items');
+        if (!Schema::hasTable('users')) Schema::create('users');
     }
 
 }
