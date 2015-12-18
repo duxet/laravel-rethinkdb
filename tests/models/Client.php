884f9eb8e9bc475c8e2a6aa1ca7785a53626971e
Karl Hughes <khughes.me@gmail.com>
2015-12-18 14:47:13 -0500

    More styling fixes

--- tests/models/Client.php
+++ tests/models/Client.php
@@ -11,10 +11,12 @@ class Client extends Model
     {
         return $this->belongsToMany('User');
     }
+
     public function photo()
     {
         return $this->morphOne('Photo', 'imageable');
     }
+
     public function addresses()
     {
         return $this->hasMany('Address', 'data.address_id', 'data.client_id');
