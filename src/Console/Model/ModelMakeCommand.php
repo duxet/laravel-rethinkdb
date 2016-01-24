f71124825bbec271aba6528a90a4312ccbe1dedb
Mhd Zaher Ghaibeh <linux.juggler@gmail.com>
2016-01-24 21:16:23 +0200

    make sure it will pickup the correct file

--- src/Console/Model/ModelMakeCommand.php
+++ src/Console/Model/ModelMakeCommand.php
@@ -45,6 +45,27 @@ class ModelMakeCommand extends LaravelMakeModelCommand
     }
 
     /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__.'/stubs/model.stub';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace;
+    }    
+
+    /**
      * Get the console command options.
      *
      * @return array
