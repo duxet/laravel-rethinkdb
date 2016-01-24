60e053836166b9ed7395c7c4b08fe49e0e69a49e
Mhd Zaher Ghaibeh <linux.juggler@gmail.com>
2016-01-24 20:04:06 +0200

    adding a model make command

---
+++ src/Console/Model/ModelMakeCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace duxet\Rethinkdb\Console\Model;
+
+use Illuminate\Foundation\Console\ModelMakeCommand as LaravelMakeModelCommand
+
+class ModelMakeCommand extends LaravelMakeModelCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:rethink-model';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new Rethinkdb model class';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Model';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function fire()
+    {
+        if (parent::fire() !== false) {
+            if ($this->option('migration')) {
+                $table = Str::plural(Str::snake(class_basename($this->argument('name'))));
+
+                $this->call('make:rethink-migration', ['name' => "create_{$table}_table", '--create' => $table]);
+            }
+        }
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['migration', 'm', InputOption::VALUE_NONE, 'Create a new migration file for the model.'],
+        ];
+    }
+}
