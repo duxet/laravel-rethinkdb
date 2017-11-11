6edf49327de4d982783db8f614834617b33c24ba
Oscar <30281971+wmtoscar@users.noreply.github.com>
2017-11-11 17:49:22 +0530

    Fix issue supportsSchemaTransactions() on null
    got this issue while fixing existing issue fix this issue 
    Call to a member function supportsSchemaTransactions() on null
    by adding Grammar.php and editing Connection.php

---
+++ src/Schema/Grammar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace duxet\RethinkDB\Schema;
+
+use Illuminate\Database\Schema\Grammars\Grammar as BaseGrammar;
+
+/**
+ * Class Grammar
+ *
+ * @package Moloquent\Schema
+ */
+class Grammar extends BaseGrammar {
+}
